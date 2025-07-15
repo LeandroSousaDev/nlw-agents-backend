7dd4f2c29779d2a3481208caadd68a66707b4aa4
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2025-07-15 18:05:45 -0300

    aula 3 feita

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,15 @@ import { env } from "./env.ts";
 import { getRoomsRoute } from "./http/routes/getRooms.ts";
 import { createRooms } from "./http/routes/createRooms.ts";
 import { getRoomQuestion } from "./http/routes/getRoomQuestion.ts";
-import { createQuestions } from "./http/routes/createQuestion.ts";
+import { createQuestion } from "./http/routes/createQuestion.ts";
+import { uploadAudio } from "./http/routes/uploadAudio.ts";
+import {fastifyMultipart} from "@fastify/multipart"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {origin: "https://localhost:5173"});
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -17,7 +21,8 @@ app.setValidatorCompiler(validatorCompiler);
 app.register(getRoomsRoute)
 app.register(createRooms)
 app.register(getRoomQuestion)
-app.register(createQuestions)
+app.register(createQuestion)
+app.register(uploadAudio)
 
 app.listen({port: env.PORT}).then(() => {
     console.log("Server is runnig!!!");
